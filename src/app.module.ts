47663138f6cb8fff9294c66061be260033533a4a
Jiwon <jiwon8518@naver.com>
2022-05-12 23:09:14 +0900

    Setting: [#7] typeORM 설정파일

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '1234',
+      database: 'task',
+      entities: [__dirname + '/apis/**/*.entity.*'],
+      synchronize: true,
+      logging: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
