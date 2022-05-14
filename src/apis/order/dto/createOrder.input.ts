25e81aeadfe57358031f2f066b05beb18c58ec07
Jiwon <jiwon8518@naver.com>
2022-05-14 17:55:54 +0900

    Feat: [#10] 주문 등록 dto 수정

--- src/apis/order/dto/createOrder.input.ts
+++ src/apis/order/dto/createOrder.input.ts
@@ -1,9 +1,10 @@
 import { IsNotEmpty } from 'class-validator';
+import { Category } from 'src/apis/category/entities/category.entity';
 
 export class CreateOrderInput {
   @IsNotEmpty()
   name: string;
 
   @IsNotEmpty()
-  category: number;
+  category: Category;
 }
